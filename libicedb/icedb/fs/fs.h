915652c544b70a3376d09a57e643f3cff5e04c06
Ryan Honeyager <ryan@honeyager.info>
2017-08-30 08:50:39 -0400

    More fs documentation.

--- libicedb/icedb/fs/fs.h
+++ libicedb/icedb/fs/fs.h
@@ -7,7 +7,8 @@
 
 ICEDB_BEGIN_DECL_C
 
-/** Filesystem handles are opaque types. They are implemented using several different backends - 
+/** @defgroup fs Filesystem
+ * Filesystem handles are opaque types. They are implemented using several different backends - 
  * for folders, for compressed archives, for netcdf and hdf5 files, et cetera.
  * To switch to a different backend, open a new handle pointing to a place along the old path. For example,
  * you can open a directory, find a netcdf file, then open it, then find a shape structure, then view 
@@ -97,28 +98,110 @@ DL_ICEDB ICEDB_fs_hnd_p ICEDB_path_open(
 	ICEDB_OPTIONAL const char* pluginid, ICEDB_OPTIONAL ICEDB_fs_hnd_p base_handle,
 	ICEDB_OPTIONAL ICEDB_file_open_flags flags, ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code*);
 
-/** \brief Get the full path opened by the file handle. 
+/** \brief Get the full path opened by the file handle.
+	*
+	* \param p is the pointer to the file handle. It must be a valid pointer.
+	* \param inPathSize is used if the output name array is already pre-allocated, in which case it represents the size (in bytes) of the array.
+		If dynamic allocation is instead requested, set this to NULL.
+	* \param outPathSize is a pointer to the number of bytes in the output array neede to represent the path. If the path is too large to fit into 
+		a staticly-allocated array, then an error code will be emitted.
+	* \param bufPath is a pointer to the output path array. Always will be null-terminated.
+	* \param deallocator is a function that can deallocate bufPath once it is no longer needed. Use this only when inPathSize != 0.
+	* \param err is an error code.
+	* \returns bufPath on success, and NULL on error.
 **/
 DL_ICEDB const char* ICEDB_fh_get_name(
-	ICEDB_fs_hnd_p, 
+	ICEDB_fs_hnd_p p, 
 	ICEDB_OPTIONAL size_t inPathSize, 
 	ICEDB_OUT size_t* outPathSize, 
-	ICEDB_OUT char ** const bufPath, 
-	ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code*);
+	ICEDB_OUT char ** const bufPath,
+	ICEDB_OPTIONAL ICEDB_OUT ICEDB_free_charIPP_f * const deallocator,
+	ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code* err);
 
-/// Get the ICEDB_file_open_flags passed to the plugin when the handle was opened
-DL_ICEDB ICEDB_file_open_flags ICEDB_fh_getOpenFlags(ICEDB_fs_hnd_p, ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code*);
+/** \brief Get the ICEDB_file_open_flags passed to the plugin when the handle was opened
+	*
+	* \param p is the pointer to the file handle. It must be a valid pointer.
+	* \param err is an error code.
+	**/
+DL_ICEDB ICEDB_file_open_flags ICEDB_fh_getOpenFlags(ICEDB_fs_hnd_p p, ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code* err);
 
-/// Close a file handle
-DL_ICEDB bool ICEDB_fh_close(ICEDB_fs_hnd_p, ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code*);
+/** \brief Close a file handle
+	*
+	* \param p is the pointer to the file handle. It must be a valid pointer.
+	* \param err is an error code.
+	**/
+DL_ICEDB bool ICEDB_fh_close(ICEDB_fs_hnd_p p, ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code*);
 
-DL_ICEDB ICEDB_error_code ICEDB_fh_move(ICEDB_fs_hnd_p, const char* src, const char* dest);
-DL_ICEDB ICEDB_error_code ICEDB_fh_copy(ICEDB_fs_hnd_p, const char* src, const char* dest, bool overwrite);
-DL_ICEDB ICEDB_error_code ICEDB_fh_unlink(ICEDB_fs_hnd_p, const char* path);
-DL_ICEDB ICEDB_error_code ICEDB_fh_create_hard_link(ICEDB_fs_hnd_p, const char* src, const char* dest);
-DL_ICEDB ICEDB_error_code ICEDB_fh_create_sym_link(ICEDB_fs_hnd_p, const char* src, const char* dest);
-DL_ICEDB ICEDB_error_code ICEDB_fh_follow_sym_link(ICEDB_fs_hnd_p,
-	const char* path, size_t out_max_sz, size_t* szout, char** out);
+/** \brief Move an object
+	*
+	* \param p is the pointer to the file handle. It must be a valid pointer.
+	* \param src is the source path. Must be a valid c-string. Cannot be NULL.
+	* \param dest is the destination path. Must be a valid c-string. Cannot be NULL.
+	* \param err is an error code.
+	* \returns True on success, false on any error.
+	**/
+DL_ICEDB bool ICEDB_fh_move(ICEDB_fs_hnd_p p, const char* src, const char* dest, ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code* err);
+
+/** \brief Copy an object
+*
+* \param p is the pointer to the file handle. It must be a valid pointer.
+* \param src is the source path. Must be a valid c-string. Cannot be NULL.
+* \param dest is the destination path. Must be a valid c-string. Cannot be NULL.
+* \param err is an error code.
+* \returns True on success, false on any error.
+**/
+DL_ICEDB bool ICEDB_fh_copy(ICEDB_fs_hnd_p p, const char* src, const char* dest, bool overwrite, ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code* err);
+
+/** \brief Unlink an object
+*
+* \param p is the pointer to the file handle. It must be a valid pointer.
+* \param src is the source path. Must be a valid c-string. Cannot be NULL.
+* \param dest is the destination path. Must be a valid c-string. Cannot be NULL.
+* \param err is an error code.
+* \returns True on success, false on any error.
+**/
+DL_ICEDB bool ICEDB_fh_unlink(ICEDB_fs_hnd_p p, const char* path, ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code* err);
+
+/** \brief Create a hard link
+*
+* \param p is the pointer to the file handle. It must be a valid pointer.
+* \param src is the source path. Must be a valid c-string. Cannot be NULL.
+* \param dest is the destination path. Must be a valid c-string. Cannot be NULL.
+* \param err is an error code.
+* \returns True on success, false on any error.
+**/
+DL_ICEDB bool ICEDB_fh_create_hard_link(ICEDB_fs_hnd_p p, const char* src, const char* dest, ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code* err);
+
+/** \brief Create a symbolic link
+*
+* \param p is the pointer to the file handle. It must be a valid pointer.
+* \param src is the source path. Must be a valid c-string. Cannot be NULL.
+* \param dest is the destination path. Must be a valid c-string. Cannot be NULL.
+* \param err is an error code.
+* \returns True on success, false on any error.
+**/
+DL_ICEDB bool ICEDB_fh_create_sym_link(ICEDB_fs_hnd_p p, const char* src, const char* dest, ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code* err);
+
+/** \brief Follow a symbolic link
+*
+* \param p is the pointer to the file handle. It must be a valid pointer.
+* \param path is the location of the symbolic link. Must be a valid c-string. Cannot be NULL.
+* \param inPathSize is used if the output name array is already pre-allocated, in which case it represents the size (in bytes) of the array.
+	If dynamic allocation is instead requested, set this to NULL.
+* \param outPathSize is a pointer to the number of bytes in the output array neede to represent the path. If the path is too large to fit into 
+	a staticly-allocated array, then an error code will be emitted.
+* \param bufPath is a pointer to the output path array.
+* \param deallocator is a function that can deallocate bufPath once it is no longer needed. Use this only when inPathSize != 0.
+* \param err is an error code.
+* \returns bufPath on success, and NULL on error.
+**/
+DL_ICEDB const char* ICEDB_fh_follow_sym_link(ICEDB_fs_hnd_p p,
+	const char* path, 
+	ICEDB_OPTIONAL size_t inPathSize,
+	ICEDB_OUT size_t* outPathSize,
+	ICEDB_OUT char ** const bufPath,
+	ICEDB_OPTIONAL ICEDB_OUT ICEDB_free_charIPP_f * const deallocator,
+	ICEDB_OPTIONAL ICEDB_OUT ICEDB_error_code* err);
 
 #define ICEDB_FS_PATH_CONTENTS_PATH_MAX 32767
 struct ICEDB_FS_PATH_CONTENTS {
@@ -138,7 +221,7 @@ DL_ICEDB ICEDB_error_code ICEDB_fh_readobjs(ICEDB_fs_hnd_p, const char* path, si
 DL_ICEDB ICEDB_error_code ICEDB_fh_free_objs(ICEDB_fs_hnd_p, ICEDB_FS_PATH_CONTENTS **);
 
 
-
+/// This is a union allowing attribute data to be accessed in different ways. Data are allocated in 64-bit blocks. Alignment depends on actual type.
 union ICEDB_attr_data {
 	int8_t* i8t;
 	int16_t* i16t;
@@ -157,18 +240,23 @@ union ICEDB_attr_data {
 	char* ct;
 };
 
+/** \brief These are the filesystem attribute types
+*
+* Most of these are equivalent to the NetCDF types. Four special types (ICEDB_type_intmax, ICEDB_type_intptr, ICEDB_type_uintmax and ICEDB_type_uintptr)
+* are internal to this library. They never get saved by themselves, but contain pointers to things like string arrays, which are represented using other NetCDF objects.
+**/
 enum ICEDB_attr_type {
-	ICEDB_type_char, // NC_CHAR
-	ICEDB_type_int8, // NC_BYTE
-	ICEDB_type_uint8, // NC_UBYTE
-	ICEDB_type_uint16, // NC_USHORT
-	ICEDB_type_int16, // NC_SHORT
-	ICEDB_type_uint32, // NC_UINT
-	ICEDB_type_int32, // NC_INT (or NC_LONG)
-	ICEDB_type_uint64, // NC_UINT64
-	ICEDB_type_int64, // NC_INT64
-	ICEDB_type_float, // NC_FLOAT
-	ICEDB_type_double, // NC_DOUBLE
+	ICEDB_type_char, ///< NC_CHAR
+	ICEDB_type_int8, ///< NC_BYTE
+	ICEDB_type_uint8, ///< NC_UBYTE
+	ICEDB_type_uint16, ///< NC_USHORT
+	ICEDB_type_int16, ///< NC_SHORT
+	ICEDB_type_uint32, ///< NC_UINT
+	ICEDB_type_int32, ///< NC_INT (or NC_LONG)
+	ICEDB_type_uint64, ///< NC_UINT64
+	ICEDB_type_int64, ///< NC_INT64
+	ICEDB_type_float, ///< NC_FLOAT
+	ICEDB_type_double, ///< NC_DOUBLE
 					   // These have no corresponding NetCDF type. They never get saved by themselves, but contain pointers to things like string arrays, which are NetCDF objects.
 					   ICEDB_type_intmax,
 					   ICEDB_type_intptr,
@@ -198,7 +286,7 @@ DL_ICEDB void ICEDB_fh_freeAttr(const ICEDB_fs_hnd_p, ICEDB_ATTR* attr, ICEDB_OU
 DL_ICEDB ICEDB_ATTR* ICEDB_fh_readAttr(const ICEDB_fs_hnd_p, const char* name, ICEDB_OUT ICEDB_error_code*);
 DL_ICEDB void ICEDB_fh_insertAttr(ICEDB_fs_hnd_p, const ICEDB_ATTR* in, ICEDB_OUT ICEDB_error_code*);
 
-
+/** @} */ // end of fs
 
 ICEDB_END_DECL_C
 #endif
