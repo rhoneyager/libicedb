b496d528e8f5957d34248325f7fad9f0fc4c63f8
Ryan Honeyager <ryan@honeyager.info>
2017-09-26 13:11:12 -0400

    I am trying top-down development. I will wriet a shape app, and then will add in all of the functions to support this app.

---
+++ apps/level_0/shapes-app.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <boost/program_options.hpp>
+#include "../../libicedb/icedb/misc/os_functions.h"
+int main(int argc, char** argv) {
+	using namespace std;
+	ICEDB_libEntry(argc, argv);
+
+	namespace po = boost::program_options;
+	po::options_description desc("Allowed options");
+
+	desc.add_options()
+		("help,h", "produce help message")
+		("input,i", po::value<vector<string> >()->multitoken(), "Input shape file(s)")
+		("output,o", po::value<string>(), "Output shape file")
+		;
+
+	po::variables_map vm;
+	po::store(po::command_line_parser(argc, argv).
+		options(desc).run(), vm);
+	po::notify(vm);
+
+	auto doHelp = [&](const std::string& s)
+	{
+		cout << s << endl;
+		cout << desc << endl;
+		exit(3);
+	};
+	if (vm.count("help") || argc <= 1) doHelp("");
+	if (!vm.count("input") || !vm.count("output")) doHelp("Must specify input and output files.");
+
+	return 0;
+}
