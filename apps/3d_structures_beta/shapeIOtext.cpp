84a52aedbdca30ed0d86ab30e930ad8093c050b7
Ryan Honeyager <ryan@honeyager.info>
2017-12-01 23:49:26 -0500

    Threaded version works, but needs optimization. W/o compression. avg cpu utilization is 2/4 cores. Shape reads are much faster than HDF5 writes, as these are mutex-protected.

--- apps/3d_structures_beta/shapeIOtext.cpp
+++ apps/3d_structures_beta/shapeIOtext.cpp
@@ -19,16 +19,37 @@ namespace icedb {
 				p.particle_scattering_element_coordinates_are_integral = this->particle_scattering_element_coordinates_are_integral;
 			}
 			void ShapeCommonOptionalData::apply(icedb::Shapes::NewShapeCommonOptionalProperties& p) const {
-				p.particle_scattering_element_number = this->particle_scattering_element_number;
-				p.particle_constituent_number = this->particle_constituent_number;
+				// When applying these, check for the trivial cases. If these occur, then do not
+				// set the output field.
+
+				// If p.particle_scattering_element_number is a linear sequence 1...N, then it is trivial
+				bool scat_elem_num_is_trivial = true;
+				for (size_t i = 0; i < this->particle_scattering_element_number.size(); ++i) {
+					if (this->particle_scattering_element_number[i] != i + 1) scat_elem_num_is_trivial = false;
+				}
+				if (!scat_elem_num_is_trivial)
+					p.particle_scattering_element_number = this->particle_scattering_element_number;
+				// Matters only if more than one constituent
+				if (this->particle_constituent_number.size() > 1)
+					p.particle_constituent_number = this->particle_constituent_number;
+
+
 				p.particle_scattering_element_radius = this->particle_scattering_element_radius;
 				p.particle_constituent_name = this->particle_constituent_name;
 				p.particle_constituent_single_name = this->particle_constituent_single_name;
-
+				
 				p.particle_scattering_element_composition_fractional = particle_scattering_element_composition_fractional;
-				p.particle_scattering_element_composition_whole = particle_scattering_element_composition_whole;
-				p.particle_scattering_element_spacing = this->particle_scattering_element_spacing;
 
+				// If p.particle_scattering_element_composition_whole is specified, and it is all ones, then it is trivial.
+				bool comp_whole_is_trivial = false;
+				if (std::all_of(particle_scattering_element_composition_whole.begin(),
+					particle_scattering_element_composition_whole.end(),
+					[](uint8_t v)->bool { return (v == 1) ? true : false; }))
+					comp_whole_is_trivial = true;
+				if (!comp_whole_is_trivial)
+					p.particle_scattering_element_composition_whole = particle_scattering_element_composition_whole;
+
+				p.particle_scattering_element_spacing = this->particle_scattering_element_spacing;
 			}
 			icedb::Shapes::Shape::Shape_Type ShapeDataBasic::toShape(
 				const std::string &name, std::shared_ptr<H5::Group> grp) const
--- apps/3d_structures_beta/shapes-main.cpp
+++ apps/3d_structures_beta/shapes-main.cpp
@@ -3,9 +3,13 @@
 // Boost bug with C++17 requires this define. See https://stackoverflow.com/questions/41972522/c2143-c2518-when-trying-to-compile-project-using-boost-multiprecision
 #define _HAS_AUTO_PTR_ETC 1
 #include <boost/program_options.hpp>
+#include <atomic>
+#include <future>
 #include <iostream>
 #include <memory>
+#include <mutex>
 #include <set>
+#include <stack>
 #include <string>
 #include <vector>
 #include <icedb/shape.hpp>
@@ -21,9 +25,51 @@ const std::map<std::string, std::set<sfs::path> > file_formats = {
 	{"icedb", {".hdf5", ".nc", ".h5", ".cdf", ".hdf"} }
 };
 
+std::mutex mHDF5; ///< Allows only one thread to write to the HDF5 file at a time.
+std::mutex mStack; ///< Allows synchronized access to the job stack
+std::stack<std::pair<sfs::path, std::string> > mystack;
+std::vector<std::unique_ptr<std::thread> > pool;
+// These get set in main(int,char**).
+float resolution_um = 0;
+bool nccompat = true;
+icedb::Groups::Group::Group_ptr basegrp;
+
+void task() {
+	// Fetch from the pool
+	while (true) {
+		decltype(mystack)::value_type cur;
+		{
+			std::lock_guard<std::mutex> lStack(mStack);
+			if (mystack.empty()) return;
+			cur = mystack.top();
+			mystack.pop();
+		}
+		auto data = icedb::Examples::Shapes::readTextFile(cur.first.string());
+		data.required.particle_id = cur.first.filename().string();
+		data.required.NC4_compat = nccompat;
+		if (resolution_um)
+			data.optional.particle_scattering_element_spacing = resolution_um / 1.e6f;
+		{
+			std::lock_guard<std::mutex> lHDF5(mHDF5);
+			std::cout << "Processing " << cur.first << std::endl;
+			auto sgrp = basegrp->createGroup(cur.second);
+			auto shp = data.toShape(cur.first.filename().string(), sgrp->getHDF5Group());
+		}
+	}
+}
+
+/// Build a pool of threads for parsing shapes and writing to the hdf5 file
+bool construct_thread_pool(int numThreads) {
+	for (int i = 0; i < numThreads; ++i)
+		pool.push_back(std::make_unique<std::thread>(task));
+	//pool.resize(numThreads, std::thread(task));
+	return true;
+}
+
 int main(int argc, char** argv) {
 	try {
 		using namespace std;
+		mStack.lock();
 		// Read program options
 		
 		namespace po = boost::program_options;
@@ -51,10 +97,14 @@ int main(int argc, char** argv) {
 		};
 		if (vm.count("help")) doHelp("");
 		if (!vm.count("from") || !vm.count("to")) doHelp("Need to specify to/from locations.");
-		
+		const int Num_Threads = thread::hardware_concurrency();
+		std::future<bool> make_threads = std::async(
+			construct_thread_pool, Num_Threads);
+
+
 		using namespace icedb;
 		int clev = vm["compression-level"].as<int>();
-		bool nccompat = vm["nc4-compat"].as<bool>();
+		nccompat = vm["nc4-compat"].as<bool>();
 		Expects(clev >= 0);
 		Expects(clev < 10);
 		icedb::fs::hdf5::useZLIB(clev);
@@ -64,7 +114,6 @@ int main(int argc, char** argv) {
 		sfs::path pFromRaw(sFromRaw);
 		sfs::path pToRaw(sToRaw);
 		string dbfolder = vm["db-folder"].as<string>();
-		float resolution_um = 0;
 		if (vm.count("resolution")) resolution_um = vm["resolution"].as<float>();
 
 		// Create the output database if it does not exist
@@ -73,19 +122,16 @@ int main(int argc, char** argv) {
 		if (vm.count("truncate")) iof = fs::IOopenFlags::TRUNCATE;
 		if (!sfs::exists(pToRaw)) iof = fs::IOopenFlags::CREATE;
 		Databases::Database::Database_ptr db = Databases::Database::openDatabase(pToRaw.string(), iof);
-		auto basegrp = db->createGroupStructure(dbfolder);
+		basegrp = db->createGroupStructure(dbfolder);
 		auto files = icedb::fs::impl::collectDatasetFiles(pFromRaw, file_formats.at("text"));
 
-		for (const auto &f : files) {
-			cout << "Creating from " << f.first << "\t" << f.second << endl;
-			auto data = icedb::Examples::Shapes::readTextFile(f.first.string());
-			data.required.particle_id = f.first.filename().string();
-			if (resolution_um)
-				data.optional.particle_scattering_element_spacing = resolution_um / 1.e6f;
-			auto sgrp = basegrp->createGroup(f.second);
-			data.required.NC4_compat = nccompat;
-			auto shp = data.toShape(f.first.filename().string(), sgrp->getHDF5Group());
-		}
+		bool threadPoolCreateRes = make_threads.get();
+		Expects(threadPoolCreateRes == true);
+
+		for (const auto &f : files) mystack.push(f);
+		mStack.unlock();
+
+		for (auto &t : pool) t->join();
 	}
 	catch (const std::exception &e) {
 		std::cerr << e.what() << std::endl;
