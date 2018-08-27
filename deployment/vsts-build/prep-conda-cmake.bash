823500f11088e94ba40d878d9b8a9ec8d67fdc77
Ryan Honeyager <ryan@honeyager.info>
2018-08-27 12:59:24 +0000

    Moving make into the conda prep script

--- deployment/vsts-build/prep-conda-cmake.bash
+++ deployment/vsts-build/prep-conda-cmake.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 export PATH="$HOME/miniconda3/bin:$PATH"
 source activate icedb
-mkdir build
+mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/icedb -DBUILD_PLUGIN_DDSCAT=off -DBUILD_PLUGIN_GENERAL_TEXT_IO=off -DBUILD_PLUGIN_PSU=on -DBUILD_PLUGIN_SILO=off -DBUILD_TESTING=off ../
+make package
 
