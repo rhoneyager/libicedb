8446a856deb5e35ea95484a8e2bb5de1047e79d5
Ryan Honeyager <ryan@honeyager.info>
2019-08-08 21:24:06 -0400

    Improving automated testing

--- cmake/Building.cmake
+++ cmake/Building.cmake
@@ -1,17 +1,22 @@
-
-include(GetGitRevisionDescription)
-# Versioning information
-get_git_head_revision(GITREFSPECVAR GITHASHVAR)
-if ("${GITREFSPECVAR}" STREQUAL "")
-	set(GITBRANCH "GIT_BRANCH_UNKNOWN")
+if(DEFINED PACKAGE_GIT_BRANCH)
+	set(GITBRANCH ${PACKAGE_GIT_BRANCH})
 else()
-	string(SUBSTRING "${GITREFSPECVAR}" 11 -1 GITBRANCH )
-endif()
 
-# This config file stores basic information about the build.
-# Version number, git hash, ...
-# This gets pulled into the icedb target.
-configure_file (
-	"${CMAKE_CURRENT_SOURCE_DIR}/lib/util/cmake-settings.h.in"
-	"${CMAKE_CURRENT_BINARY_DIR}/lib/icedb-cmake-settings.h"
-    )
+	include(GetGitRevisionDescription)
+	# Versioning information
+	get_git_head_revision(GITREFSPECVAR GITHASHVAR)
+	if ("${GITREFSPECVAR}" STREQUAL "")
+		set(GITBRANCH "GIT_BRANCH_UNKNOWN")
+	else()
+		string(SUBSTRING "${GITREFSPECVAR}" 11 -1 GITBRANCH )
+	endif()
+
+	# This config file stores basic information about the build.
+	# Version number, git hash, ...
+	# This gets pulled into the icedb target.
+	configure_file (
+		"${CMAKE_CURRENT_SOURCE_DIR}/lib/util/cmake-settings.h.in"
+		"${CMAKE_CURRENT_BINARY_DIR}/lib/icedb-cmake-settings.h"
+	    )
+
+endif()
