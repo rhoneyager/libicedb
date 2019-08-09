024ccbfac89478abb244668330795cb928a14da4
Ryan Honeyager <ryan@honeyager.info>
2019-08-08 20:16:36 -0400

    .

---
+++ CTestConfig.cmake
@@ -0,0 +1,16 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+
+set(CTEST_PROJECT_NAME "icedb")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
+set(CTEST_DROP_METHOD "https")
+#set(CTEST_DROP_SITE "")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=icedb")
+set(CTEST_DROP_SITE_CDASH TRUE)
+
