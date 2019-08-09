0bce3c70aa5bf1d972ef50864a0fd395d2a2e776
Ryan Honeyager <ryan@honeyager.info>
2019-08-08 21:27:21 -0400

    Amending previous commit

--- cmake/Building.cmake
+++ cmake/Building.cmake
@@ -10,13 +10,14 @@ else()
 	else()
 		string(SUBSTRING "${GITREFSPECVAR}" 11 -1 GITBRANCH )
 	endif()
-
-	# This config file stores basic information about the build.
-	# Version number, git hash, ...
-	# This gets pulled into the icedb target.
-	configure_file (
-		"${CMAKE_CURRENT_SOURCE_DIR}/lib/util/cmake-settings.h.in"
-		"${CMAKE_CURRENT_BINARY_DIR}/lib/icedb-cmake-settings.h"
-	    )
-
 endif()
+
+# This config file stores basic information about the build.
+# Version number, git hash, ...
+# This gets pulled into the icedb target.
+configure_file (
+	"${CMAKE_CURRENT_SOURCE_DIR}/lib/util/cmake-settings.h.in"
+	"${CMAKE_CURRENT_BINARY_DIR}/lib/icedb-cmake-settings.h"
+    )
+
+
